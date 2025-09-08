6acdbb09e26ff7bd8419843470e9456fc68d7923
malish-oleg <v14dt20@yandex.ru>
2025-09-09 02:15:10 +0300

    Update vite.config.js

--- schedule-frontend/vite.config.js
+++ schedule-frontend/vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      // Строка '/api' будет заменена на 'http://localhost:3001'
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    }
+  }
 })
