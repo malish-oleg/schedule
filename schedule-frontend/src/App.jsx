96b75c9fd908c729b31719df49f6045353b78d14
malish-oleg <v14dt20@yandex.ru>
2025-09-09 01:01:05 +0300

    Add files via upload

--- schedule-frontend/src/App.jsx
+++ schedule-frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import SchedulePage from './pages/SchedulePage';
+import ScheduleRedirector from './pages/ScheduleRedirector';
 import './App.css';
 
 function App() {
@@ -9,6 +10,8 @@ function App() {
       <div className="container">
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/schedule/:facultyId/:groupId" element={<ScheduleRedirector />} />
+          
           <Route path="/schedule/:facultyId/:groupId/:week" element={<SchedulePage />} />
         </Routes>
       </div>
