60895b67c13508d80a15f1c3693dde400f9d49df
malish-oleg <v14dt20@yandex.ru>
2025-09-09 16:28:13 +0300

    Delete vercel speed analytics

--- schedule-frontend/src/App.jsx
+++ schedule-frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import SchedulePage from './pages/SchedulePage';
-import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/next";
 import './App.css';
 
@@ -16,7 +15,6 @@ function App() {
         </Routes>
       </div>
       <Analytics />
-      <SpeedInsights />
     </BrowserRouter>
   );
 }
