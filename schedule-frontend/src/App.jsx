2dafec0e0b7ee231a397b5a0ed3b38e6467eab6a
malish-oleg <v14dt20@yandex.ru>
2025-09-09 16:17:00 +0300

    Add vercel analytics

--- schedule-frontend/src/App.jsx
+++ schedule-frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import SchedulePage from './pages/SchedulePage';
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next";
 import './App.css';
 
 function App() {
@@ -13,6 +15,8 @@ function App() {
           <Route path="/schedule/:facultyId/:groupId/:week?" element={<SchedulePage />} />
         </Routes>
       </div>
+      <Analytics />
+      <SpeedInsights />
     </BrowserRouter>
   );
 }
