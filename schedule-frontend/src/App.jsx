1662b25dbbeb22a256d960311edf51255cc6b623
malish-oleg <v14dt20@yandex.ru>
2025-09-12 21:50:03 +0300

    del modal

--- schedule-frontend/src/App.jsx
+++ schedule-frontend/src/App.jsx
@@ -1,38 +1,13 @@
-import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import SchedulePage from './pages/SchedulePage';
 import { Analytics } from "@vercel/analytics/react";
 import Footer from './components/Footer';
-import UpdateModal from './components/UpdateModal';
 import './App.css';
 
-const UPDATE_MODAL_STORAGE_KEY = 'updateModalDismissed-v1';
-
 function App() {
-
-  const [showUpdateModal, setShowUpdateModal] = useState(false);
-
-  useEffect(() => {
-    // Проверяем, была ли отметка в localStorage
-    const hasDismissed = localStorage.getItem(UPDATE_MODAL_STORAGE_KEY);
-    
-    // Если отметки нет, показываем окно
-    if (!hasDismissed) {
-      setShowUpdateModal(true);
-    }
-  }, []);
-
-  const handleCloseUpdateModal = () => {
-    // При закрытии ставим отметку в localStorage
-    localStorage.setItem(UPDATE_MODAL_STORAGE_KEY, 'true');
-    setShowUpdateModal(false);
-  };
-
-
   return (
     <BrowserRouter>
-      {showUpdateModal && <UpdateModal on_Close={handleCloseUpdateModal} />}
       <div className="app-layout">
         <main className="main-content">
           <div className="container">
@@ -45,6 +20,7 @@ function App() {
         </main>
         <Footer />
       </div>
+      <Analytics />
     </BrowserRouter>
   );
 }
