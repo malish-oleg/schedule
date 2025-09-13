0b864d47ca5b4980f9eb7c201e953a6d0c137fba
malish-oleg <v14dt20@yandex.ru>
2025-09-13 10:23:43 +0300

    Add update modal

--- schedule-frontend/src/App.jsx
+++ schedule-frontend/src/App.jsx
@@ -1,13 +1,38 @@
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import SchedulePage from './pages/SchedulePage';
 import { Analytics } from "@vercel/analytics/react";
+import UpdateModal from './components/UpdateModal';
 import Footer from './components/Footer';
 import './App.css';
 
+const UPDATE_MODAL_STORAGE_KEY = 'updateModalDismissed-v2';
+
 function App() {
+
+  const [showUpdateModal, setShowUpdateModal] = useState(false);
+
+  useEffect(() => {
+    // Проверяем, была ли отметка в localStorage
+    const hasDismissed = localStorage.getItem(UPDATE_MODAL_STORAGE_KEY);
+    
+    // Если отметки нет, показываем окно
+    if (!hasDismissed) {
+      setShowUpdateModal(true);
+    }
+  }, []);
+
+  const handleCloseUpdateModal = () => {
+    // При закрытии ставим отметку в localStorage
+    localStorage.setItem(UPDATE_MODAL_STORAGE_KEY, 'true');
+    setShowUpdateModal(false);
+  };
+
   return (
     <BrowserRouter>
+
+      {showUpdateModal && <UpdateModal on_Close={handleCloseUpdateModal} />}
       <div className="app-layout">
         <main className="main-content">
           <div className="container">
